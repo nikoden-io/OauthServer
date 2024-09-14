2d30f522008482f67193b54afc2890f715011a72
Nicolas Denoël <github@nikoden.io>
2024-09-14 18:47:42 +0200

    feat(token): Add refresh token capability

--- OauthServer.Presentation/Config.cs
+++ OauthServer.Presentation/Config.cs
@@ -21,16 +21,33 @@ public class Config
     public static IEnumerable<Client> Clients =>
         new List<Client>
         {
-            new()
+            new Client
             {
                 ClientId = "client1",
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
                 ClientSecrets = { new Secret("secret".Sha256()) },
+
+                // Enable refresh tokens
+                // AllowAccessTokenViaBrowser = false, => Gives an error unknown
+                AllowOfflineAccess = true,
+
+                // Enable token revocation
+                RequireClientSecret = true,
+                
+                AccessTokenLifetime = 3600, // 1 hour
+                RefreshTokenUsage = TokenUsage.OneTimeOnly,
+                RefreshTokenExpiration = TokenExpiration.Absolute,
+                AbsoluteRefreshTokenLifetime = 2592000, // 30 days in seconds
+
                 AllowedScopes =
                 {
-                    "api1", IdentityServerConstants.StandardScopes.OpenId,
-                    IdentityServerConstants.StandardScopes.Profile
+                    "api1",
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.OfflineAccess // Needed for refresh tokens
                 }
             }
         };
+
 }
